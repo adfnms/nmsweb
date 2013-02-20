98b90d5fac91d923ed42b097ca3fbeb44346152c
kichokicho <kicho@adflow.co.kr>
2013-02-20 09:39:46 +0900

    nodeController.java 수정

--- nmsweb/src/main/java/kr/co/adflow/nms/web/NodeController.java
+++ nmsweb/src/main/java/kr/co/adflow/nms/web/NodeController.java
@@ -36,7 +36,7 @@ public class NodeController {
 
 	@RequestMapping(value = "/nodes", method = RequestMethod.GET)
 	public @ResponseBody
-	String nodesAll(Locale locale, Model model) {
+	String nodes(Locale locale, Model model) {
 
 		NodeProcess controll = NodeProcess.getProcess();
 		String result = null;
@@ -51,7 +51,7 @@ public class NodeController {
 	}
 
 	@RequestMapping(value = "/nodes/{id}", method = RequestMethod.GET)
-	public @ResponseBody String serchNode(@PathVariable String id ){
+	public @ResponseBody String nodeId(@PathVariable String id ){
 
 		NodeProcess controll = NodeProcess.getProcess();
 		String result = null;
@@ -68,7 +68,146 @@ public class NodeController {
 
 		return result;
 	}
+	
+	@RequestMapping(value = "/nodes/{id}/ipinterfaces", method = RequestMethod.GET)
+	public @ResponseBody String nodeIdIpinterfaces(@PathVariable String id ){
 
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesIpInterfaces(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	
+	@RequestMapping(value = "/nodes/{id}/ipinterfaces/{ipAddress}", method = RequestMethod.GET)
+	public @ResponseBody String nodeIdIpinterfaces(@PathVariable String id, @PathVariable String ipAddress ){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesIpInterfaces(id, ipAddress);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/nodes/{id}/ipinterfaces/{ipAddress}/services", method = RequestMethod.GET)
+	public @ResponseBody String nodeIdIpinterfacesService(@PathVariable String id, @PathVariable String ipAddress ){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesIpInterfacesServices(id, ipAddress);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/nodes/{id}/ipinterfaces/{ipAddress}/services/{serviceName}", method = RequestMethod.GET)
+	public @ResponseBody String nodeIdIpinterfacesService(@PathVariable String id, @PathVariable String ipAddress, @PathVariable String serviceName){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesIpInterfacesServices(id, ipAddress, serviceName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/nodes/{id}/snmpinterfaces", method = RequestMethod.GET)
+	public @ResponseBody String nodeSnmpinterfaces(@PathVariable String id){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesSnmpinterfaces(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	
+	@RequestMapping(value = "/nodes/{id}/snmpinterfaces/{ifIndex}", method = RequestMethod.GET)
+	public @ResponseBody String nodeSnmpinterfaces(@PathVariable String id, @PathVariable String ifIndex){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesSnmpinterfaces(id, ifIndex);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/nodes/{id}/categories", method = RequestMethod.GET)
+	public @ResponseBody String nodeCategories(@PathVariable String id){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesCategories(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/nodes/{id}/categories/{categoryName}", method = RequestMethod.GET)
+	public @ResponseBody String nodeCategories(@PathVariable String id, @PathVariable String categoryName){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesCategories(id,categoryName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/nodes/{id}/assetRecord", method = RequestMethod.GET)
+	public @ResponseBody String nodeAssetRecord(@PathVariable String id){
+
+		NodeProcess controll = NodeProcess.getProcess();
+		String result = null;
+
+		try {
+			result = (String) controll.nodesCategories(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	
+	
 	/**
 	 * sendNewSuspectEvent
 	 * 
