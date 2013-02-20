72fe21e4f9dbb478eed846fdfc4475666d2397ad
typark <typark@adflow.co.kr>
2013-02-20 14:47:04 +0900

    Update nmsweb/src/main/java/kr/co/adflow/nms/web/TestController.java
    version update 2.0

--- nmsweb/src/main/java/kr/co/adflow/nms/web/TestController.java
+++ nmsweb/src/main/java/kr/co/adflow/nms/web/TestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * TestController
  * 
  * @author typark@adflow.co.kr
- * @version 1.5
+ * @version 2.0
  */
 @Controller
 public class TestController {
