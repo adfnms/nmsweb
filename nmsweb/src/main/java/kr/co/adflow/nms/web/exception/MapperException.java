ba184ef94e61c6b38e9b7f804c3df46948223034
kichokicho <kicho@adflow.co.kr>
2013-02-22 16:38:40 +0900

    test

---
+++ nmsweb/src/main/java/kr/co/adflow/nms/web/exception/MapperException.java
@@ -0,0 +1,58 @@
+package kr.co.adflow.nms.web.exception;
+
+/**
+ * This exception indicates an error has occurred handling data from opennms
+ * rest service
+ */
+public class MapperException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * <p>
+	 * Constructor for HandleException.
+	 * </p>
+	 */
+	public MapperException() {
+		super();
+	}
+
+	/**
+	 * <p>
+	 * Constructor for HandleException.
+	 * </p>
+	 */
+	public MapperException(String message) {
+		super(message);
+	}
+
+	/**
+	 * <p>
+	 * Constructor for HandleException.
+	 * </p>
+	 * 
+	 * @param message
+	 *            a {@link java.lang.String} object.
+	 * @param cause
+	 *            a {@link java.lang.Throwable} object.
+	 */
+	public MapperException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * <p>
+	 * Constructor for HandleException.
+	 * </p>
+	 * 
+	 * @param cause
+	 *            a {@link java.lang.Throwable} object.
+	 */
+	public MapperException(Throwable cause) {
+		super(cause);
+	}
+
+}
