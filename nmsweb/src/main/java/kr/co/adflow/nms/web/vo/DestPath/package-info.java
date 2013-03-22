ab42f89c585127da2e1ae541036891272b7e8485
kichokicho <kicho@adflow.co.kr>
2013-03-22 11:47:28 +0900

    3/22 - vo package-info.java delete

--- nmsweb/src/main/java/kr/co/adflow/nms/web/vo/DestPath/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// �� ������ JAXB(JavaTM Architecture for XML Binding) ���� ���� 2.2.6 ������ ���� ��Ǿ���ϴ�. 
-// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>�� �����Ͻʽÿ�. 
-// �� ������ �����ϸ� �ҽ� ��Ű���� ���������� �� ���� ������ �սǵ˴ϴ�. 
-// �� ��¥: 2013.02.25 �ð� 06:29:50 PM KST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlns.opennms.org/xsd/destinationPaths", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package kr.co.adflow.nms.web.vo.DestPath;
