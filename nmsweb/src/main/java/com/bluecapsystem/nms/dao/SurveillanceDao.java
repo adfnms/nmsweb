2071854d5c600fa4cae0973adc4e7c4130efac51
pauseinsert3 <pauseinsert3@gmail.com>
2013-08-19 17:51:59 +0900

    Update SurveillanceDao.java

--- nmsweb/src/main/java/com/bluecapsystem/nms/dao/SurveillanceDao.java
+++ nmsweb/src/main/java/com/bluecapsystem/nms/dao/SurveillanceDao.java
@@ -3,17 +3,12 @@ package com.bluecapsystem.nms.dao;
 import java.util.List;
 
 import com.bluecapsystem.nms.dto.AssetsTbl;
+import com.bluecapsystem.nms.dto.CategoriesTbl;
 import com.bluecapsystem.nms.dto.CategoryNodeTbl;
 
 public interface SurveillanceDao 
 {
+	boolean getCategoriesName(List<CategoriesTbl> CategoriesItem);	
 	
-	
-	boolean getRouters(List<CategoryNodeTbl> routersInfo);	
-
-	boolean getSwitches(List<CategoryNodeTbl> switchesInfo);
-
-	boolean getServers(List<CategoryNodeTbl> serversInfo);
-	
-	boolean getNodeId(List<CategoryNodeTbl> nodeId);	
+	Integer getCount(Integer categoryId);
 }
